7a93b31fb93b1f700ef3cca76f7a8534ca1b2043
Brian Smith <brian.smith365@gmail.com>
2017-08-23 17:38:36 -0400

    trip pickup assoc

---
+++ db/migrate/20170823211717_add_trip_to_pickup.rb
@@ -0,0 +1,5 @@
+class AddTripToPickup < ActiveRecord::Migration[5.1]
+  def change
+    add_reference :pickups, :trip, foreign_key: true
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170823202001) do
+ActiveRecord::Schema.define(version: 20170823211717) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,6 +20,8 @@ ActiveRecord::Schema.define(version: 20170823202001) do
     t.integer "status", default: 0
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "trip_id"
+    t.index ["trip_id"], name: "index_pickups_on_trip_id"
   end
 
   create_table "transactions", force: :cascade do |t|
@@ -44,4 +46,5 @@ ActiveRecord::Schema.define(version: 20170823202001) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "pickups", "trips"
 end
