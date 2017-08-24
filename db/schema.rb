a81c40ebc458f7b2e6589299d7c61815fb93b277
Brian Smith <brian.smith365@gmail.com>
2017-08-24 15:11:33 -0400

    added password_digest to users and added routes /signup /users and added form to users new view

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170824144355) do
+ActiveRecord::Schema.define(version: 20170824170324) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -42,6 +42,7 @@ ActiveRecord::Schema.define(version: 20170824144355) do
     t.integer "credit_count"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "password_digest"
   end
 
   add_foreign_key "pickups", "trips"
