3fbdac08bb2483cbd25d6e8c32fbbb4eefd1bd16
Alex Jean-Baptiste <alxjeanbaptiste@gmail.com>
2017-08-25 17:11:01 -0400

    described creating a new user and logging in to the service

--- spec/features/users_spec.rb
+++ spec/features/users_spec.rb
@@ -1,5 +1,18 @@
 require 'rails_helper'
 
 RSpec.feature "Users", type: :feature do
-  pending "add some scenarios (or delete) #{__FILE__}"
+  describe "to create a new user" do
+    it "I should be able to go to the homepage and see the link to signup as a new user"
+    it "should be able to fill out the sign up form to create an account"
+    it "should redirect to homepage and see a welcome message only after creating an account"
+    it "should verify that a name/email/address/password/password confirmation image was uploaded"
+    it "should verify that email is unique"
+  end
+
+  describe "login to the service" do
+    it "I should be able to go to the homepage and see the link to login"
+    it "should be able to fill out the login form to sign in to my account"
+    it "should redirect to homepage and see a welcome message only after successfully logging into my account"
+    it "should verify that the email/password exist"
+  end
 end
