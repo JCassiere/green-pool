e4ef7d1fd901b4894e15d20c4ecf763e2720a9eb
Alex Jean-Baptiste <alxjeanbaptiste@gmail.com>
2017-08-25 17:39:15 -0400

    I described the tests for the homepage feature

--- spec/features/hompages_spec.rb
+++ spec/features/hompages_spec.rb
@@ -1,5 +1,8 @@
 require 'rails_helper'
 
 RSpec.feature "Hompages", type: :feature do
-  pending "add some scenarios (or delete) #{__FILE__}"
+  it "I should be able to go the page and see the start trip link"
+  it "I should be able to to go to the page and see the find trip link"
+  it "I should be able to go to the new trip page after I click the start trip button only if i am logged in"
+  it "I should be able to go to the find trip page after I click the find trip button only if i am logged in"
 end
