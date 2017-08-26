f8aa52f0e44598d9dc3b0fa99fbf4ab6cd308b11
Alex Jean-Baptiste <alxjeanbaptiste@gmail.com>
2017-08-26 16:26:19 -0400

    changed users view folder to just user, passed second test in userpages_spec.rb

--- spec/features/userpages_spec.rb
+++ spec/features/userpages_spec.rb
@@ -8,8 +8,9 @@ RSpec.feature "Userpages", type: :feature do
   end
 
   it "userpage should load if logged in" do
-    visit '/users/1'
-    expect(page).to have_content('Hello, User!')
+    user = User.create!(name: Faker::Name.name, email: Faker::Internet.email, address: "#{Faker::Address.street_address} #{Faker::Address.city},#{Faker::Address.state_abbr} #{Faker::Address.zip}", credit_count: 3, password_digest: Faker::DragonBall.character)
+    visit "/users/#{user.id}"
+    expect(page).to have_content("Hello, #{user.name}!" )
   end
   it "userpage should show user's trips"
   it "userpage should show pending trips"
