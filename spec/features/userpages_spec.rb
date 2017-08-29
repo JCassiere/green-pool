ac0d046c294fbd5d4156f01eae5cb43e4c2692aa
Alex Jean-Baptiste <alxjeanbaptiste@gmail.com>
2017-08-29 17:56:05 -0400

    passed the fifth and sixth tests

--- spec/features/userpages_spec.rb
+++ spec/features/userpages_spec.rb
@@ -38,8 +38,20 @@ RSpec.feature "Userpages", type: :feature do
       expect(page).to have_content("Pick ups pending: #{@user.driver_pickups.last.id}")
     end
 
+    it "recycler page should show pending pickups" do
+      @user.trips.create(pickup_time: Faker::Date.forward(1), total_space: 3)
+      recycler = User.create!(name: Faker::Name.name, email: Faker::Internet.email, address: "#{Faker::Address.street_address} #{Faker::Address.city},#{Faker::Address.state_abbr} #{Faker::Address.zip}", credit_count: 3, password_digest: Faker::DragonBall.character)
+      @user.trips.last.pickups.create(num_bags: 3, status: 0, user: recycler)
+      visit user_path(recycler)
+      expect(page).to have_content("Pick ups pending: #{recycler.recycling_pickups.last.id}")
+    end
 
-    it "userpage should show number of credits"
+
+    it "userpage should show number of credits" do
+      visit user_path(@user)
+      expect(page).to have_content("Credit count: #{@user.credit_count}")
+    end
+
     it "userpage should allow user to logout and be redirected to homepage"
 
   end
