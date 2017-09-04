0ead668b3e1988b24a726b7283028d62f443e404
Alex Jean-Baptiste <alxjeanbaptiste@gmail.com>
2017-09-04 10:34:32 -0400

    passed finalconfirmations_spec.rb post merge 9/3

--- spec/features/finalconfirmations_spec.rb
+++ spec/features/finalconfirmations_spec.rb
@@ -8,11 +8,11 @@ RSpec.feature "Finalconfirmations", type: :feature do
     @pickup = @trip.pickups.create(num_bags: 3,status: 0, user_id: @recycler.id)
     visit trip_path(@trip)
     click_button("Accept")
-    visit trip_path(@trip)
+    # visit trip_path(@trip)
   end
 
   describe "confirming a finished transaction on a recyclers page" do
-    it "should find a complete link on the pickup after a driver accepts the pickup " do
+    it "should find a complete button on the pickup after a driver accepts the pickup " do
       expect(find_button("Complete").visible?).to be true
     end
 
@@ -26,9 +26,10 @@ RSpec.feature "Finalconfirmations", type: :feature do
       expect(find_button("Done").visible?).to be true
     end
     it "after the driver clicks the done button on the confirmation modal the pickup should change to pending" do
-      click_button("Complete")      
+      click_button("Complete")
       expect(find_button("Done").visible?).to be true
       click_button("Done")
+      save_and_open_page
       expect(page).to have_content("Pending")
     end
 
