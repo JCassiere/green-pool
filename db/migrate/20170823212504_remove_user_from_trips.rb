149c3204293f4055310d8ecc6fb0cd13feef6eaa
Alex Jean-Baptiste <alxjeanbaptiste@gmail.com>
2017-09-04 19:16:58 -0400

    removed offending migrations

--- db/migrate/20170823212504_remove_user_from_trips.rb
+++
@@ -1,5 +0,0 @@
-class RemoveUserFromTrips < ActiveRecord::Migration[5.1]
-  def change
-    remove_column :trips, :user_id, :bigint
-  end
-end
--- db/migrate/20170823212646_remove_user_from_pickups.rb
+++
@@ -1,5 +0,0 @@
-class RemoveUserFromPickups < ActiveRecord::Migration[5.1]
-  def change
-    remove_column :pickups, :user_id, :bigint
-  end
-end
